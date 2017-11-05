b41b0027e35d66001c5d39c9a32e59bc052eb1d5
Mitali Kaushal <mitali.kaushal9@gmail.com>
2017-11-05 01:15:52 -0700

    adding functionality

--- rolladicegame.js
+++ rolladicegame.js
@@ -1,6 +1,6 @@
 var scores = [0,0];
 var roundScore = 0;
-var activePlayer = 1;
+var activePlayer = 0;
 
 //hiding the dices
 document.querySelector(".dice").style.display = 'none';
@@ -17,6 +17,27 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 	var diceDOM = document.querySelector(".dice")
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-' + dice + '.png';
+	//update the round if the tolled number is not 1
+	if(dice !== 1){
+		//Add Score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	}
+	else {
+		//Next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = "0";
+		document.getElementById('current-1').textContent = "0";
+
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		document.querySelector('.dice').style.display = 'none';
+
+	}
 
 
 });
